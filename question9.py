042e4c5367802c1f059d0ec5ed1a56e680717886
kalkidanfekadu <jogenet81@gmail.com>
2025-12-10 04:32:54 -0800

    update

--- question9.py
+++ question9.py
@@ -1,6 +1,11 @@
 num=[1,2,3,4,5,6,7,8]
+found=False
 target=int(input("Enter an intrger target number"))
 for i in range(len(num)-1):
   for j in range(i+1,len(num)):
     if num[i]+num[j]==target and num[i]!=num[j]:
         print([i,j])
+        found=True
+if found==False:
+ print("Not found")
+    
